01e57c366314cf10a66800e72d5fc41f6ab7b53c
Den Williams <dennis.williams@loke.com.au>
2016-06-06 21:48:11 +1000

    initial version - IR support only

---
+++ README.md
@@ -0,0 +1,10 @@
+# Node-RED Orvibo AllOne node
+This package contains a node to send IR via an Orvibo AllOne
+
+## Installation
+Use `npm install node-red-contrib-orvibo-allone` to install.
+
+## Usage
+The node appears in the 'devices' category in your Node-Red palette.
+
+Use the config node to set the IP and MAC address of your AllOne.
---
+++ nodes/orvibo.js
@@ -0,0 +1,79 @@
+var Orvibo = require('node-orvibo');
+
+function registerOrviboNodes(RED) {
+
+  /* ---------------------------------------------------------------------------
+   * CONFIG node
+   * -------------------------------------------------------------------------*/
+  function OrviboNodeConfig(config) {
+    RED.nodes.createNode(this, config);
+
+    // Configuration options passed by Node Red
+    this.name = config.name;
+
+    // Config node state
+    this.orvibo = new Orvibo();
+    var device = {
+      macAddress: config.mac,
+      macPadding: '202020202020',
+      type: 'AllOne',
+      ip: config.ip,
+      name: 'Allone',
+      password: '888888',
+      icon: '00'
+    };
+    this.orvibo.subscribe(device);
+
+    // Define functions called by nodes
+    var node = this;
+    this.emitIR = function (irCode) {
+      node.orvibo.emitIR(device, irCode);
+    };
+
+    // Define config node event listeners
+    node.on("close", function(done){
+      node.remote = null;
+      done();
+    });
+  }
+  RED.nodes.registerType("orvibo", OrviboNodeConfig);
+
+
+  /* ---------------------------------------------------------------------------
+   * EMITIR node
+   * -------------------------------------------------------------------------*/
+  function OrviboNodeEmitIR(config) {
+    RED.nodes.createNode(this, config);
+
+    // Save settings in local node
+    this.device = config.device;
+    this.deviceNode = RED.nodes.getNode(this.device);
+    this.name = config.name;
+    this.code = config.code;
+
+    var node = this;
+    if (this.deviceNode) {
+
+      // Input handler, called on incoming flow
+      this.on('input', function(msg) {
+
+        // If no key is given in the config, then we us the key in the msg.
+        var code = (node.code) ? node.code : msg.payload;
+        if (!code) {
+          node.error('No code given. Specify either in the config or via msg.payload!');
+          return;
+        }
+
+        // Put data to the device.
+        node.deviceNode.emitIR(String(code));
+      });
+
+    } else {
+      this.error(RED._("orvibo.errors.missing-config"));
+    }
+  }
+  RED.nodes.registerType("orvibo-emitir", OrviboNodeEmitIR);
+
+}
+
+module.exports = registerOrviboNodes;
